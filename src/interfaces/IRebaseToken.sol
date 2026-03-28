46969d9fd3626a7bd7cfaaed4d6586e2af48a294
Tymek <tymek_uramek@wp.pl>
2026-03-28 20:15:34 +0100

    FEAT: IREbaseToken.sol

--- src/interfaces/IRebaseToken.sol
+++ src/interfaces/IRebaseToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 
 
 interface IRebaseToken {
-    function mint(address account, uint256 amount) external;
-    function burn(address account, uint256 amount) external;
+    function mint(address _to, uint256 amount) external;
+    function burn(address _from, uint256 amount) external;
     
 }
