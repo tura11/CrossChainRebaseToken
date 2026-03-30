86f47c6ba4ea2c952d7a275cfa26045667990da0
Tymek <tymek_uramek@wp.pl>
2026-03-30 15:34:17 +0200

    Update interface

--- src/interfaces/IRebaseToken.sol
+++ src/interfaces/IRebaseToken.sol
@@ -6,5 +6,6 @@ pragma solidity ^0.8.24;
 interface IRebaseToken {
     function mint(address _to, uint256 amount) external;
     function burn(address _from, uint256 amount) external;
+    function balanceOf(address _account) external view returns (uint256);
     
 }
