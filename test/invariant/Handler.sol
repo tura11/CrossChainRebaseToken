d26f1832bbca88e659d777110f3e1c1600fd3491
Tymek <tymek_uramek@wp.pl>
2026-03-31 09:37:35 +0200

    fix: compile issue

--- test/invariant/Handler.sol
+++ test/invariant/Handler.sol
@@ -3,26 +3,22 @@
 pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
-import {StdInvariant} from "forge-std/StdInvariant.sol";
 import {RebaseToken} from "../../src/RebaseToken.sol";
 import {Vault} from "../../src/Vault.sol";
 import {IRebaseToken} from "../../src/interfaces/IRebaseToken.sol";
 
 
-contract Handler is StdInvariant, Test {
+contract Handler is Test {
     RebaseToken token;
     Vault vault;
-    uint256 totalMinted;
-    uint256 totalBurned;
+    uint256 public totalMinted;
+    uint256 public totalBurned;
     address[] actors = new address[](3);
 
 
-    constructor(address _token) {
+    constructor(address _token, address _vault) {
         token = RebaseToken(_token);
-        vault = new Vault(IRebaseToken(address(token)));
-        address owner = address(this);
-        vm.prank(owner);
-        token.grantMintAndBurnRole(address(vault));
+        vault = Vault(payable(_vault));
         actors[0] = makeAddr("actor0");
         actors[1] = makeAddr("actor1");
         actors[2] = makeAddr("actor2");
@@ -43,11 +39,9 @@ contract Handler is StdInvariant, Test {
         address actor = actors[bound(actorIndex, 0, actors.length - 1)];
         uint256 maxAmountToWithdraw = token.balanceOf(actor);
         if(maxAmountToWithdraw < 1e5) return;
-        amount = bound(amount, 1e5,maxAmountToWithdraw );
+        amount = bound(amount, 1e5, maxAmountToWithdraw);
         vm.prank(actor);
         vault.redeem(amount);
         totalBurned += amount;
     }
-
-
-}   
+}
