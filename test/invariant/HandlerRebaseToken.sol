f3e9e66b1a3189b191d8e7cf570a4f657941f2a5
TymekU <tura11@users.noreply.github.com>
2026-04-01 13:15:05 +0200

    Feat:Mint

--- test/invariant/HandlerRebaseToken.sol
+++ test/invariant/HandlerRebaseToken.sol
@@ -9,15 +9,28 @@ import {RebaseToken} from "../../src/RebaseToken.sol";
 
 contract HandlerRebaseToken is Test {
     RebaseToken token; 
-    address user;
+    address[] users = new address[](3);
     address owner;
     uint256 public totalMinted;
     uint256 public totalBurned;
 
     constructor(address _token) {
         token = RebaseToken(_token);
-        user = makeAddr("user");
+        users[0] = makeAddr("user1");
+        users[1] = makeAddr("user2");
+        users[2] = makeAddr("user3");
         owner = makeAddr("owner");
+    }
+
+
+
+    function mint(uint256 amount, uint256 actorIndex) public {
+        address user = users[bound(actorIndex, 0, users.length - 1)];
+        amount = bound(amount, 1e5, type(uint96).max);
+        vm.deal(user, amount);
+        vm.prank(user);
+        token.mint(user, amount);
+        totalMinted += amount;
     }
 
 
