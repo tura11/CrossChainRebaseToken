2c57e0c593580ceeca78de8dfb81605a1b0c887f
Tymek <tymek_uramek@wp.pl>
2026-04-03 16:39:00 +0200

    FEAT:Mint and Burn functions

--- src/RebaseTokenPool.sol
+++ src/RebaseTokenPool.sol
@@ -2,18 +2,35 @@
 
 pragma solidity ^0.8.24;
 
-import {Pool} from "@ccip/contracts/src/v0.8/ccip/libraries/Pool.sol";
-import {TokenPool} from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Pool} from "@ccip/ccip/libraries/Pool.sol";
+import {TokenPool} from "@ccip/ccip/pools/TokenPool.sol";
+import {IERC20} from "@ccip/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
 
 
 contract RebaseTokenPool is TokenPool {
     constructor(IERC20 _token, address[] memory _allowlist, address _rnmProxy, address _router) TokenPool(_token, _allowlist, _rnmProxy, _router){}
 
 
-    function lockOrBurn(Pool.LockOrBurnInV1 calldata lockOrBurnIn) public returns (Pool.LockOrBurnOutV1 memory lockOrBurnOut);
+    function lockOrBurn(Pool.LockOrBurnInV1 calldata lockOrBurnIn) public returns (Pool.LockOrBurnOutV1 memory lockOrBurnOut){
+        _validateLockOrBurn(lockOrBurnIn);
+        uint256 userInterestRate = IRebaseToken(address(i_token)).getUserInterestRate(lockOrBurnIn.originalSender);
+        IRebaseToken(address(i_token)).burn(address(this), lockOrBurnIn.amount);
+        lockOrBurnOut = Pool.LockOrBurnOutV1({
+            destTokenAddress: getRemoteToken(lockOrBurnIn.remoteChainSelector),
+            destPoolData: abi.encode(userInterestRate)
+        });
+
+    }
 
 
-    function releaseOrMint(Pool.ReleaseOrMintInV1 calldata releaseOrMintIn) public returns (Pool.ReleaseOrMintOutV1 memory);
+    function releaseOrMint(Pool.ReleaseOrMintInV1 calldata releaseOrMintIn) public returns (Pool.ReleaseOrMintOutV1 memory){
+        _validateReleaseOrMint(releaseOrMintIn);
+        uint256 userInterestRate = abi.decode(releaseOrMintIn.sourcePoolData, (uint256));
+        IRebaseToken(address(i_token)).mint(releaseOrMintIn.receiver, releaseOrMintIn.amount, userInterestRate);
+        return Pool.ReleaseOrMintOutV1({
+            destinationAmount: releaseOrMintIn.amount
+        });
+    }
 
 }
