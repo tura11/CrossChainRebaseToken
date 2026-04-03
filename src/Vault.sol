fb209dbccdafc195e474a0552aca702bd83c460e
Tymek <tymek_uramek@wp.pl>
2026-04-03 16:39:24 +0200

    Refactor: mint function

--- src/Vault.sol
+++ src/Vault.sol
@@ -20,7 +20,8 @@ contract Vault {
 
 
     function deposit() external payable {
-        i_rebaseToken.mint(msg.sender, msg.value);
+        uint256 interestRate = i_rebaseToken.getInterestRate();
+        i_rebaseToken.mint(msg.sender, msg.value, interestRate);
         emit Deposited(msg.sender, msg.value);
 
     }
