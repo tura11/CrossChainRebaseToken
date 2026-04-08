86b18f3423004c09dfaf659fb2f517752aa3f6b3
Tymek <tymek_uramek@wp.pl>
2026-04-08 07:27:59 +0200

    Feat:Deployer.sol

---
+++ script/Deployer.s.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+import {Script} from "forge-std/src/Script.sol";
+import {Vault} from "../src/Vault.sol";
+import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
+import {RebaseToken} from "../src/RebaseToken.sol";
+import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
+import {IERC20} from "@ccip/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {CCIPLocalSimulatorFork} from "@chainlink/local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {Register} from "@chainlink/local/src/ccip/Register.sol";
+import {RegistryModuleOwnerCustom} from "@ccip/ccip/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
+
+contract TokenAndPoolDeployer is Script{
+    
+    function run() public returns(RebaseToken token, RebaseTokenPool pool){
+        CCIPLocalSimulatorFork ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        Register.NetworkDetails networkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
+
+        vm.startBroadcast();
+        token = new RebaseToken();
+        pool = new RebaseTokenPool(IERC20(address(token)), new address[](0), networkDetails.rmnProxyAddress, networkDetails.routerAddress);
+        token.grantMintAndBurnRole(address(pool));
+        RegistryModuleOwnerCustom(networkDetails.registryModuleOwnerCustomAddress).registerAdminViaOwner(address(token));
+        TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress).acceptAdminRole(address(token));
+        TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress).setPool(address(token), address(pool));
+        vm.stopBroadcast();
+    }
+
+}
+
+
+contract VaultDeployer is Script{
+
+    function run(address _rebaseToken) public returns(Vault vault){
+        vm.startBroadcast();
+        vault = new Vault(IRebaseToken(_rebaseToken));
+        IRebaseToken(_rebaseToken).grantMintAndBurnRole(address(vault));
+        vm.stopBroadcast();
+    }
+}
